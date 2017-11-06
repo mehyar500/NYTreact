30b9086bf2b69a43807d459dad52472a89a803e9
mehyar500 <mehyar500@gmail.com>
2017-11-06 10:57:47 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+const App = () =>
+    <Wrapper>
+        <p>NY Times</p>
+    </Wrapper>;
+
+export default App;
