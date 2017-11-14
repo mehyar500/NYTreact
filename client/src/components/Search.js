49d2ed8ddb2dc15fd30ab5a09053cf76ee0c2d2b
mehyar500 <mehyar500@gmail.com>
2017-11-13 20:03:39 -0500

    Date Picker Feature

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import * as bootstrap from "react-bootstrap";
+import * as moment from "moment";
+import DateRangePicker from "react-bootstrap-daterangepicker";
 import API from "./Utils/API";
+
 
 class Search extends Component {
   state = {
     query: "",
+    startDate: moment().subtract(29, 'days'),
+    endDate: moment(),
     articles: [],
     saved: []
   };
 
   componentDidMount() {
-    API.get("Breaking")
+    API.get("Latest", this.state.startDate, this.state.endDate)
       .then(res => {
         console.log(res);
         this.setState({ articles: res });
@@ -18,25 +24,31 @@ class Search extends Component {
   }
 
   handleInputCahnge = event => {
+    console.log(event.target);
     const { name, value } = event.target;
     this.setState({
       [name]: value
     });
+  };    
+
+  handleDateChange = (startDate, endDate) => {
+    const { startDate, endDate } = this.state;
+      console.log("startDate: ", startDate, ", endDate: ", endDate);
+      return this.setState({ startDate: startDate, endDate: endDate });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    return API.get(this.state.query)
-      .then(res => {
-        this.setState({ articles: res });
-        console.log(this.state.articles);
-      })
-      .catch(err => console.log(err));
+    return API.get(this.state.query, this.state.startDate, this.state.endDate)
+        .then(res => {
+          this.setState({ articles: res });
+          console.log(this.state.articles);
+        })
+        .catch(err => console.log(err))
   };
 
   render() {
-    return (
-      <div className="container">
+    return <div className="container">
         <div className="row">
           <div style={{ height: 300 }} className="jumbotron">
             <h1>New York Times Articles Search</h1>
@@ -44,38 +56,14 @@ class Search extends Component {
           <form>
             <div className="form-group">
               <h4>Topic</h4>
-              <input
-                type="text"
-                placeholder="Search For Articles."
-                className="form-control"
-                value={this.state.query}
-                name="query"
-                onChange={this.handleInputCahnge}
-              />
+              <input type="text" placeholder="Search For Articles." className="form-control" value={this.state.query} name="query" onChange={this.handleInputCahnge} />
             </div>
             <div className="form-group">
-              <h4>Start Year</h4>
-              <div class="input-group date" id="datetimepicker1">
-                <input type="text" class="form-control" />
-                <span class="input-group-addon">
-                  <span class="glyphicon glyphicon-calendar" />
-                </span>
-              </div>
+              <DateRangePicker startDate={moment("6/10/1986")} endDate={moment("3/1/2014")}>
+                <div>Pick Date Range!</div>
+              </DateRangePicker>
             </div>
-            <div className="form-group">
-              <div class="input-group date" id="datetimepicker1">
-                <h4>End Year</h4>
-                <input type="text" class="form-control" />
-                <span class="input-group-addon">
-                  <span class="glyphicon glyphicon-calendar" />
-                </span>
-              </div>
-            </div>
-            <button
-              className="btn btn-primary"
-              type="button"
-              onClick={this.handleSubmit}
-            >
+            <button className="btn btn-primary" type="button" onClick={this.handleSubmit}>
               Search
             </button>
           </form>
@@ -86,30 +74,24 @@ class Search extends Component {
               <h3>Articles List</h3>
             </div>
             <div className="panel-body">
-              {this.state.articles.length ? (
-                <div className="list-overflow-container">
+              {this.state.articles.length ? <div className="list-overflow-container">
                   <ul className="list-group">
                     {this.state.articles.map(article => {
-                      return (
-                        <li className="list-group-item" key={article._id}>
+                      return <li className="list-group-item" key={article._id} date={article.pub_date}>
                           <a href={article.web_url}>
-                            <h3>{article.headline.main}</h3>
+                            <h3>Headline: {article.headline.main}</h3>
+                            <h4>Published: {article.pub_date}</h4>
                             <p>{article.snippet}</p>
                           </a>
                           <button className="btn btn-block">Save</button>
-                        </li>
-                      );
+                        </li>;
                     })}
                   </ul>
-                </div>
-              ) : (
-                <h3>No Results to Display</h3>
-              )}
+                </div> : <h3>No Results to Display</h3>}
             </div>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
--- client/src/components/Utils/API.js
+++ client/src/components/Utils/API.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
 export default {
-    get: query => {
+    get: (query, startYear, endYear) => {
         const queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        return axios.get(queryURLBase, {params: {q:query, "api-key": authKey}})
+        return axios.get(queryURLBase, {params: {q:query, "api-key": authKey, "begin_date": startYear, "end_date": endYear}})
                     .then(res =>{ console.log(res.data.response.docs); return res.data.response.docs;})
                     .catch(err => console.log(err));
     }
